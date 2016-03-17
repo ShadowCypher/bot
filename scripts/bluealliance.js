4a68abb31a9b53bb67a502fa3deaf0fbe30b6948
Tyler Carter <tyler@tylercarter.org>
2016-03-17 13:59:16 -0700

    Add blue alliance responders

---
+++ scripts/bluealliance.js
@@ -0,0 +1,41 @@
+module.exports = function(robot) {
+  var TBA = require( './tba-api.js');
+  robot.hear( /what is the blue alliance\s?/, function ( message ){
+    message.send( "The Blue Alliance is a site where anyone can view basic information and statistics on FIRST FRC Teams. http://thebluealliance.com." );
+    message.send( TBA.version() );
+  });
+
+  robot.respond( /who is team (\d*)\s?/i, function ( message ){
+    var team = message.match[1].replace("?","");
+    TBA.team.get( "frc"+team, function( info ){
+
+      if ( info.nickname === undefined ) {
+        message.reply(
+          "There is no team with that number."
+        );
+      } else {
+        message.reply(
+          "Team " + team + " (" + info.nickname + ")"
+          + " from " + info.location
+          + " started in " + info.rookie_year + "."
+          + " Their website is " + info.website
+        );
+      }
+    } );
+  });
+
+  robot.respond( /when is event (.*)\s?/i, function ( message ){
+    var event = message.match[1].replace("?","");
+    TBA.event.get( event, function( info ){
+      if ( info.name === undefined ) {
+        message.reply(
+          "There is no event like that."
+        );
+      } else {
+        message.reply(
+          "The " + info.name + " is from " + info.start_date + " until " + info.end_date + "."
+        );
+      }
+    } );
+  });
+};
