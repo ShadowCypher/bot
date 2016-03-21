58bd6e19a0341382f5a06ac6b7e0925cee89f470
Tyler Carter <tyler@tylercarter.org>
2016-03-21 07:54:51 -0700

    clean up regex

--- scripts/utils.js
+++ scripts/utils.js
@@ -8,15 +8,15 @@
 //  hubot give me a quote - Responds with a random quote
 //  ... how many/much ... - Outputs random integer(from 1-20) to answer a quantitative question.
 module.exports = function(robot) {
-  robot.hear( /is it tuesday\s?\s?/i, function ( response ){
+  robot.hear( /is it tuesday/i, function ( response ){
     var today = new Date();
     response.reply( today.getDay() === 2 ? "IT IS TUESDAY!!!!!!!" : "Nope. Not Tuesday Yet." );
   } );
-  robot.hear( /is it the weekend\s?\s?/i, function ( response ){
+  robot.hear( /is it the weekend/i, function ( response ){
     var today = new Date();
     response.reply( today.getDay() === 0 || today.getDay() === 6 ? "It is the freaken weekend." : "Nope. Get back to work." );
   } );
-  robot.hear( /how (much|many)\s?\s?/i, function( response ) {
+  robot.hear( /how (much|many)/i, function( response ) {
     response.reply( Math.floor(Math.random() * (20)) + 1);
   } );
   robot.respond( /give me (.*)carrot fact(s)?/i, function( response ) {
